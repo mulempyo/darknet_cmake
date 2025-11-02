c385d81c68352fc003df88aad62f69f9e5c8fd66
KWJ <69792115+mulempyo@users.noreply.github.com>
2025-11-02 15:44:36 +0900

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-# ROS2 환경
 if [ -f /opt/ros/foxy/install/setup.bash ]; then
   source /opt/ros/foxy/install/setup.bash
 fi
 
-# 워크스페이스로 이동
 cd /colcon_ws
 
-# 빌드
-colcon build --packages-select darknet_ros darknet_ros_3d
+colcon build --packages-up-to darknet_ros darknet_ros_3d
 
-# 워크스페이스 환경 반영
 if [ -f ./install/setup.bash ]; then
   source ./install/setup.bash
 fi
