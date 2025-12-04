007c38a23df1ce7630ded0153dacf65469765f39
KWJ <69792115+mulempyo@users.noreply.github.com>
2025-12-04 17:49:22 +0900

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ fi
 
 cd /colcon_ws
 
-colcon build --packages-up-to darknet_ros darknet_ros_3d
+colcon build --packages-up-to kwj_description darknet_ros_msgs gb_visual_detection_3d_msgs darknet_ros darknet_ros_3d mpc_ros
 
 if [ -f ./install/setup.bash ]; then
   source ./install/setup.bash
